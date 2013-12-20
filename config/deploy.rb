c0f72a4ee25ea09754a27e7059c73f325300df1f
jony malhotra <jony@idifysolutions.com>
2013-12-20 15:30:46 +0530

    cap file new changes

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,68 +1,39 @@
 require "bundler/capistrano"
 
-# require 'airbrake/capistrano'
-
 server = "50.116.56.223" 
 
 set :application, "IdifyShop"
 set :repository,  "git@github.com:idify/idify_shop.git"
-#set :port, 6969
 
-set :use_sudo,  false
-
-set :scm, "git"
-set :user,      'root'#'myweightworld'
-set :password,  '' #'MYweight404'
-
-set :keep_releases, 5
-set :branch, "master"
-
-set :rails_env, 'production'
-
-set :deploy_to, "/var/www/IdifyShop" #"/home/#{user}/apps/#{application}"
+# via the :deploy_to variable:
+set :deploy_to, "/var/local/#{application}"
+ 
+# If you aren't using Subversion to manage your source code, specify
+# your SCM below:
+set :scm, :git 
 set :deploy_via, :remote_cache
-
-default_run_options[:pty] = true
-
-role :web, server
-role :app, server
-role :db,  server, :primary => true
-
-after "deploy:update_code", "deploy:symlink_config"
-after "deploy:update_code", "deploy:generate_tinymce_cache"
-after 'deploy:update_code', "deploy:precompile_assets"
-after "deploy",             "deploy:cleanup"
-# after "deploy",             "deploy:build_missing_paperclip_styles"
-
-
-
-#after "deploy:symlink", "deploy:update_crontab"
-
-
+set :branch, "master"
+ 
+set :passenger_conf, true
+set :user, "root" # Login as?
+set :use_sudo, false
+ 
+ 
+role :web, domain                          # Your HTTP server, Apache/etc
+role :app, domain                          # This may be the same as your `Web` server
+role :db,  domain, :primary => true        # This is where Rails migrations will run
+#role :db,  "your slave db-server here"
+ 
+set :rails_env, "production"
+ 
+# If you are using Passenger mod_rails uncomment this:
+# if you're still using the script/reapear helper you will need
+# these http://github.com/rails/irs_process_scripts
+ 
 namespace :deploy do
-  desc "build missing paperclip styles"
-  task :build_missing_paperclip_styles, :roles => :app do
-    run "cd #{release_path}; RAILS_ENV=production bundle exec rake paperclip:refresh:missing_styles"
-  end
-  
-  task :precompile_assets, :roles => :app do
-    run "cd #{release_path} && rm -rf public/assets/*"
-    run "cd #{release_path} && RAILS_ENV=production bundle exec rake assets:precompile"
-  end
-  
-  task :symlink_config, :roles => :app, :except => {:no_release => true, :no_symlink => true} do
-    run "ln -nsf #{shared_path}/config/database.yml #{current_release}/config"
-  end
-  
-  task :generate_tinymce_cache, :roles => :app do
-    run "cd #{current_release} && RAILS_ENV=production bundle exec rake tinymce:cache_js"
-  end
-  
+  task :start do ; end
+  task :stop do ; end
   task :restart, :roles => :app, :except => { :no_release => true } do
     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
   end
 end
-
-
-
-
